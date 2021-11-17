0a064a2a4c172a73a6fff6276e70043388283cfc
VenelinDimitrov <65946212+VenelinDimitrov@users.noreply.github.com>
2021-11-17 19:10:02 +0100

    Real Deal task - Spring Data

---
+++ RealDeal/Real Deal_Skeleton/RealDeal_Skeleton/src/main/java/softuni/exam/models/dto/xml/OfferRootSeedDto.java
@@ -0,0 +1,26 @@
+package softuni.exam.models.dto.xml;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.math.BigDecimal;
+import java.util.List;
+
+@XmlRootElement(name = "offers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class OfferRootSeedDto {
+
+    @XmlElement(name = "offer")
+    private List<OfferSeedDto> offers;
+
+    public List<OfferSeedDto> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(List<OfferSeedDto> offers) {
+        this.offers = offers;
+    }
+}
